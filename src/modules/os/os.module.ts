e4213573ab037c3d98bdd7084cad884949a7de8d
Your Name <leonardorocha.limaa@gmail.com>
2023-04-11 21:21:24 -0300

    Corrigindo o erro do osModule

--- src/modules/os/os.module.ts
+++ src/modules/os/os.module.ts
@@ -3,14 +3,15 @@ import { PrismaService } from '../prisma/prisma.service';
 import { HttpModule } from '@nestjs/axios';
 import { OsService } from './os.service';
 import { OsController } from './os.controller';
-import { ServicesService } from '../services/services.service';
 import { ProdctModule } from '../product/product.module';
 import { ServiceModule } from '../services/services.module';
+import { ServicesService } from '../services/services.service';
+import { ProductService } from '../product/product.service';
 
 @Module({
   controllers: [OsController],
-  providers: [OsService, PrismaService, ServiceModule, ProdctModule],
+  providers: [OsService, PrismaService, ServicesService, ProductService],
   exports: [OsService],
   imports: [HttpModule]
 })
-export class OsModule {}
+export class OsModule { }
