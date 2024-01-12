e9fae350f0178d12955313c23a780c955cc200f9
Higor Clem Seixas <higor.c.seixas@gmail.com>
2024-01-11 22:39:33 -0300

    feat: add pagination in getUsers

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,124 +1,142 @@
 import {
-    Controller,
-    Get,
-    InternalServerErrorException,
-    HttpCode,
-    HttpStatus,
-    Post,
-    Put,
-    Req,
-    Body,
-    Delete,
-    HttpException,
-    UseGuards
+  Controller,
+  Get,
+  InternalServerErrorException,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Put,
+  Req,
+  Body,
+  Delete,
+  HttpException,
+  UseGuards,
+  Query,
 } from '@nestjs/common';
-import { UserInterface } from '../../interfaces/userInterface';
-import { UserService } from './user.service';
+
 import { JWTServiceGuard } from '../guards/services.guard';
+
+import { UserInterface } from '../../interfaces/userInterface';
+
+import { GetUserDto } from './dto/user.dto';
+
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService: UserService) { }
+  constructor(private readonly userService: UserService) {}
 
-    @Get('getAllUsers')
-    @UseGuards(JWTServiceGuard)
-    @HttpCode(HttpStatus.OK)
-    async getUsers() {
-        return await this.userService.getAllUsers()
-            .then((result) => result)
-            .catch((error) => {
-                console.log(error);
-                throw new InternalServerErrorException();
-            })
+  @Get('getAllUsers')
+  @UseGuards(JWTServiceGuard)
+  @HttpCode(HttpStatus.OK)
+  async getUsers(
+    @Query('page') page: number = 1,
+    @Query('pageSize') pageSize: number = 10,
+    @Query() getUserDto: GetUserDto,
+  ) {
+    try {
+      const result = await this.userService.getAllUsers(
+        page,
+        pageSize,
+        getUserDto,
+      );
+      return result;
+    } catch (error) {
+      throw new InternalServerErrorException();
     }
+  }
 
-    @Post('createUser')
-    @UseGuards(JWTServiceGuard)
-    @HttpCode(HttpStatus.OK)
-    async createUser(@Body() body) {
-        const user: UserInterface = {
-            name: body.name,
-            cpf: body.cpf,
-            cellphone: body.cellphone,
-            password: body.password,
-            typeId: body.typeId
-        }
-        return await this.userService.createUser(user)
-            .then((result) => result)
-            .catch((error) => {
-                console.log(error);
-                throw new InternalServerErrorException();
-            })
-    }
+  @Post('createUser')
+  @UseGuards(JWTServiceGuard)
+  @HttpCode(HttpStatus.OK)
+  async createUser(@Body() body) {
+    const user: UserInterface = {
+      name: body.name,
+      cpf: body.cpf,
+      cellphone: body.cellphone,
+      password: body.password,
+      typeId: body.typeId,
+    };
+    return await this.userService
+      .createUser(user)
+      .then((result) => result)
+      .catch((error) => {
+        console.log(error);
+        throw new InternalServerErrorException();
+      });
+  }
 
-    @Get('getUserById')
-    @UseGuards(JWTServiceGuard)
-    @HttpCode(HttpStatus.OK)
-    async getUserById(@Body() body) {
-        return await this.userService.getUserById(body.id)
-            .then((result) => result)
-            .catch((error) => {
-                console.log(error);
-                throw new InternalServerErrorException();
-            })
-    }
+  @Get('getUserById')
+  @UseGuards(JWTServiceGuard)
+  @HttpCode(HttpStatus.OK)
+  async getUserById(@Body() body) {
+    return await this.userService
+      .getUserById(body.id)
+      .then((result) => result)
+      .catch((error) => {
+        console.log(error);
+        throw new InternalServerErrorException();
+      });
+  }
 
-    @Get('getUser')
-    @UseGuards(JWTServiceGuard)
-    @HttpCode(HttpStatus.OK)
-    async getUser(@Body() body) {
-        return await this.userService.getUser(body.cpf)
-            .then((result) => result)
-            .catch((error) => {
-                console.log(error);
-                throw new InternalServerErrorException();
-            })
-    }
+  @Get('getUser')
+  @UseGuards(JWTServiceGuard)
+  @HttpCode(HttpStatus.OK)
+  async getUser(@Body() body) {
+    return await this.userService
+      .getUser(body.cpf)
+      .then((result) => result)
+      .catch((error) => {
+        console.log(error);
+        throw new InternalServerErrorException();
+      });
+  }
 
-    @Put('updateUser')
-    @UseGuards(JWTServiceGuard)
-    @HttpCode(HttpStatus.OK)
-    async updateUser(@Req() req) {
-        const user: UserInterface = {
-            name: req.query.name,
-            cpf: req.query.cpf,
-            cellphone: req.query.cellphone,
-            password: req.query.password,
-            typeId: req.query.typeId
-        }
-        return await this.userService.updateUser(user)
-            .then((result) => result)
-            .catch((error) => {
-                console.log(error);
-                throw new InternalServerErrorException();
-            })
-    }
+  @Put('updateUser')
+  @UseGuards(JWTServiceGuard)
+  @HttpCode(HttpStatus.OK)
+  async updateUser(@Req() req) {
+    const user: UserInterface = {
+      name: req.query.name,
+      cpf: req.query.cpf,
+      cellphone: req.query.cellphone,
+      password: req.query.password,
+      typeId: req.query.typeId,
+    };
+    return await this.userService
+      .updateUser(user)
+      .then((result) => result)
+      .catch((error) => {
+        console.log(error);
+        throw new InternalServerErrorException();
+      });
+  }
 
-    @Delete('deleteUser')
-    @UseGuards(JWTServiceGuard)
-    @HttpCode(HttpStatus.OK)
-    async deteleUser(@Req() req) {
-        return await this.userService.deleteUser(req.query.cpf)
-            .then((result) => result)
-            .catch((error) => {
-                console.log(error);
-                throw new InternalServerErrorException();
-            })
-    }
+  @Delete('deleteUser')
+  @UseGuards(JWTServiceGuard)
+  @HttpCode(HttpStatus.OK)
+  async deteleUser(@Req() req) {
+    return await this.userService
+      .deleteUser(req.query.cpf)
+      .then((result) => result)
+      .catch((error) => {
+        console.log(error);
+        throw new InternalServerErrorException();
+      });
+  }
 
-    
-    @Get('getUserFromToken')
-    @UseGuards(JWTServiceGuard)
-    @HttpCode(HttpStatus.OK)
-    async getUserFromToken(@Req() req): Promise<UserInterface> {
-        const token = req.headers.authorization.split(' ')[1];
-        return this.userService.getUserFromToken(token)
-          .then((result) => {
-            return result;
-          })
-          .catch((error) => {
-            throw new HttpException(error.message, error.status);
-          });
-    }
-
+  @Get('getUserFromToken')
+  @UseGuards(JWTServiceGuard)
+  @HttpCode(HttpStatus.OK)
+  async getUserFromToken(@Req() req): Promise<UserInterface> {
+    const token = req.headers.authorization.split(' ')[1];
+    return this.userService
+      .getUserFromToken(token)
+      .then((result) => {
+        return result;
+      })
+      .catch((error) => {
+        throw new HttpException(error.message, error.status);
+      });
+  }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,152 +1,169 @@
 import { HttpService } from '@nestjs/axios';
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { UserInterface } from '../../interfaces/userInterface';
-import { PrismaService } from '../prisma/prisma.service';
-import { hash } from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+
+import { hash } from 'bcryptjs';
+
+import { GetUserDto } from './dto/user.dto';
+
+import { UserInterface } from '../../interfaces/userInterface';
+
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-    constructor(
-        private prisma: PrismaService,
-        private readonly httpService: HttpService,
-        private readonly jwtService: JwtService,
-    ) { }
+  constructor(
+    private prisma: PrismaService,
+    private readonly httpService: HttpService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-    async getAllUsers() {
-        return await this.prisma.user.findMany({
-            select: {
-                id: true,
-                name: true,
-                cpf: true,
-                cellphone: true,
-                userType: true,
-                typeId: true,
-                scheduling: true,
-                os: true,
-                createdAt: true,
-                updatedAt: true,
-            }
-        })
+  async getAllUsers(page: number, pageSize: number, getUserDto: GetUserDto) {
+    const { orderBy } = getUserDto;
+
+    const skip = (page - 1) * pageSize;
+
+    return await this.prisma.user.findMany({
+      select: {
+        id: true,
+        name: true,
+        cpf: true,
+        cellphone: true,
+        userType: true,
+        typeId: true,
+        scheduling: true,
+        os: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+      skip,
+      take: pageSize,
+      orderBy: {
+        [orderBy || 'id']: 'asc',
+      },
+    });
+  }
+
+  async createUser(requestUser: UserInterface) {
+    return await this.getUser(requestUser.cpf)
+      .then(async (user) => {
+        if (!user) {
+          const hashedPassword = await hash(requestUser.password, 10);
+          const typeIdInt = parseInt(requestUser.typeId.toString());
+          return this.prisma.user
+            .create({
+              data: {
+                name: requestUser.name,
+                cpf: requestUser.cpf.replace(/[^a-zA-Z0-9]/g, ''),
+                password: hashedPassword,
+                cellphone: requestUser.cellphone,
+                typeId: typeIdInt,
+              },
+            })
             .then((result) => result)
             .catch((error) => {
-                console.log(error)
-                throw new Error(error.message)
-            })
-    }
-
-    async createUser(requestUser: UserInterface) {
-        return await this.getUser(requestUser.cpf)
-            .then(async (user) => {
-                if (!user) {
-                    const hashedPassword = await hash(requestUser.password, 10)
-                    const typeIdInt = parseInt(requestUser.typeId.toString())
-                    return this.prisma.user.create({
-                        data: {
-                            name: requestUser.name,
-                            cpf: requestUser.cpf.replace(/[^a-zA-Z0-9]/g, ''),
-                            password: hashedPassword,
-                            cellphone: requestUser.cellphone,
-                            typeId: typeIdInt
-                        }
-                    })
-                        .then((result) => result)
-                        .catch((error) => {
-                            console.log(error)
-                            throw new Error('Erro ao criar usuário!')
-                        })
-                } else {
-                    throw new Error('Usuário já existe na base de dados!')
-                }
-            })
-            .catch((error) => {
-                console.log(error)
-                throw new Error(error.message)
-            })
-    }
-
-    async getUserById(id: number) {
-        return await this.prisma.user.findUnique({ where: { id: id } })
-            .then((result) => result)
-            .catch((error) => {
-                console.error(error)
-                throw new Error(error.message)
-            })
-    }
-
-    async getUser(cpf: string) {
-        return await this.prisma.user.findUnique({ where: { cpf: cpf } })
-            .then((result) => result)
-            .catch((error) => {
-                console.error(error)
-                throw new Error(error.message)
-            })
-    }
-
-    async updateUser(requestUser: UserInterface) {
-        return await this.getUser(requestUser.cpf)
-            .then(async (user) => {
-                if (!user) {
-                    throw new Error('Usuário não encontrado!')
-                } else {
-                    return await this.prisma.user.update({
-                        where: { cpf: requestUser.cpf },
-                        data: {
-                            name: requestUser.name,
-                            cellphone: requestUser.cellphone,
-                            typeId: requestUser.typeId
-                        }
-                    })
-                        .then((result) => result)
-                        .catch((error) => {
-                            console.log(error)
-                            throw new Error(error.message)
-                        })
-                }
-            })
-            .catch((error) => {
-                console.log(error)
-                throw new Error(error.message)
-            })
-    }
-
-    async deleteUser(cpf: string) {
-        return await this.getUser(cpf)
-            .then(async (user) => {
-                if (!user) {
-                    throw new Error('Usuário não encontrado!')
-                } else {
-                    return await this.prisma.user.delete({ where: { cpf: cpf } })
-                        .then((result) => result)
-                        .catch((error) => {
-                            console.log(error)
-                            throw new Error(error.message)
-                        })
-                }
-            })
-            .catch((error) => {
-                console.log(error)
-                throw new Error(error.message)
-            })
-    }
-
-    async getUserFromToken(token: string): Promise<UserInterface> {
-        return this.jwtService.verifyAsync(token)
-            .then(async (payload) => {
-                const userInfo = await this.getUserById(payload.sub)
-                if (userInfo) {
-                    const user = { ...userInfo };
-                    delete user.password;
-
-                    return user;
-                } else {
-                    throw new HttpException('Usuário não encontrado!', HttpStatus.UNAUTHORIZED);
-                }
-            })
-            .catch((error) => {
-                console.log(error);
-                throw new HttpException('Token inválido', HttpStatus.UNAUTHORIZED);
+              console.log(error);
+              throw new Error('Erro ao criar usuário!');
             });
-    }
+        } else {
+          throw new Error('Usuário já existe na base de dados!');
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        throw new Error(error.message);
+      });
+  }
 
+  async getUserById(id: number) {
+    return await this.prisma.user
+      .findUnique({ where: { id: id } })
+      .then((result) => result)
+      .catch((error) => {
+        console.error(error);
+        throw new Error(error.message);
+      });
+  }
+
+  async getUser(cpf: string) {
+    return await this.prisma.user
+      .findUnique({ where: { cpf: cpf } })
+      .then((result) => result)
+      .catch((error) => {
+        console.error(error);
+        throw new Error(error.message);
+      });
+  }
+
+  async updateUser(requestUser: UserInterface) {
+    return await this.getUser(requestUser.cpf)
+      .then(async (user) => {
+        if (!user) {
+          throw new Error('Usuário não encontrado!');
+        } else {
+          return await this.prisma.user
+            .update({
+              where: { cpf: requestUser.cpf },
+              data: {
+                name: requestUser.name,
+                cellphone: requestUser.cellphone,
+                typeId: requestUser.typeId,
+              },
+            })
+            .then((result) => result)
+            .catch((error) => {
+              console.log(error);
+              throw new Error(error.message);
+            });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        throw new Error(error.message);
+      });
+  }
+
+  async deleteUser(cpf: string) {
+    return await this.getUser(cpf)
+      .then(async (user) => {
+        if (!user) {
+          throw new Error('Usuário não encontrado!');
+        } else {
+          return await this.prisma.user
+            .delete({ where: { cpf: cpf } })
+            .then((result) => result)
+            .catch((error) => {
+              console.log(error);
+              throw new Error(error.message);
+            });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        throw new Error(error.message);
+      });
+  }
+
+  async getUserFromToken(token: string): Promise<UserInterface> {
+    return this.jwtService
+      .verifyAsync(token)
+      .then(async (payload) => {
+        const userInfo = await this.getUserById(payload.sub);
+        if (userInfo) {
+          const user = { ...userInfo };
+          delete user.password;
+
+          return user;
+        } else {
+          throw new HttpException(
+            'Usuário não encontrado!',
+            HttpStatus.UNAUTHORIZED,
+          );
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        throw new HttpException('Token inválido', HttpStatus.UNAUTHORIZED);
+      });
+  }
 }
